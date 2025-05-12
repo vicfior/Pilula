9d0cdcd6ac50ea2e85aaa71131e8dbc1e220cc10
victoria <victoria.fior2001@gmail.com>
2025-05-12 19:21:34 -0300

    Adicionando tela de alarme

---
+++ src/pages/Alarme/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import styles from './styles';
+import { useNavigation } from '@react-navigation/native';
+import {ArrowLeft, HouseSimple} from 'phosphor-react-native';
+
+const Alarme = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Alarme</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default Alarme;
